09df9205af88001d4e0e3e18d68c392b6057d809
JinSooo <69420819+JinSooo@users.noreply.github.com>
2021-08-12 22:49:53 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '878d251b6d8148889b719934f0e47124',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Personal Blog',
+  domain: 'https://www.notion.so/jinso/JinSo-7dcc9279c2604034be929fecfa4ae166',
+  author: 'JinSo',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -18,7 +18,7 @@ module.exports = {
 
   // social usernames (optional)
   twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
+  github: 'https://github.com/JinSooo',
   linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
